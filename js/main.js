087def704363f9844729669347e13456606636c8
karthi <pkarthip91@gmail.com>
2021-09-09 17:35:46 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -445,3 +445,19 @@ alert(isFirefox)
 //     alert('firefox')
 //     document.getElementById("imgSize")[0].setAttribute("ar-modes", "webxr scene-viewer quick-look");
 // }
+
+
+
+var browser = (function (agent) {
+    switch (true) {
+        case agent.indexOf("edge") > -1: return "MS Edge (EdgeHtml)";
+        case agent.indexOf("edg") > -1: return "MS Edge Chromium";
+        case agent.indexOf("opr") > -1 && !!window.opr: return "opera";
+        case agent.indexOf("chrome") > -1 && !!window.chrome: return "chrome";
+        case agent.indexOf("trident") > -1: return "Internet Explorer";
+        case agent.indexOf("firefox") > -1: return "firefox";
+        case agent.indexOf("safari") > -1: return "safari";
+        default: return "other";
+    }
+})(window.navigator.userAgent.toLowerCase());
+document.body.innerHTML =  "This is " + browser + " browser." + "<br><br>" + window.navigator.userAgent.toLowerCase();
