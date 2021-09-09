61703d978ff5cbb8ac740d805a72b3403df23b89
karthi <pkarthip91@gmail.com>
2021-09-09 14:38:10 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -405,6 +405,10 @@ var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.user
 // Firefox 1.0+
 var isFirefox = typeof InstallTrigger !== 'undefined';
 
+if(isFirefox == true){
+    document.getElementById("imgSize")[0].setAttribute("ar-modes", "webxr scene-viewer quick-look");
+}
+
 // Safari 3.0+ "[object HTMLElementConstructor]" 
 var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
 
@@ -420,7 +424,7 @@ var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.r
 // Blink engine detection
 var isBlink = (isChrome || isOpera) && !!window.CSS;
 
-alert(isFirefox, "Yes")
+// alert(isFirefox, "Yes")
 
 // let firefoxAgent = 
 // userAgentString.indexOf("Firefox") > -1;
